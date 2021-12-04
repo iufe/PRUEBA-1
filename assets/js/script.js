7a0bb7f1d6ae2d8d445d617bb8fb69f0cafa2072
iufe <eugenia.alic@gmail.com>
2021-12-04 06:00:11 -0300

    primer commit de terminado

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+
+    $("a").click(function () {
+        var gato = this.hash
+
+        $("html, body").animate(
+            {
+                scrollTop: $(gato).offset().top
+            },  
+            1000
+        )
+    })
+})  
